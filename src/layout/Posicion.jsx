d185b8002e5b3497ca5fac0ad89501571edbedd2
dantermax117 <dantermax117@gmail.com>
2025-02-21 18:08:37 -0600

    Ejercicios

--- src/layout/Posicion.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Posicion = () => {
-  return (
-    <div>Posicion</div>
-  )
-}
-
-export default Posicion
--- src/layout/Posicion_compresion.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Posicion_compresion = () => {
-  return (
-    <div>Posicion_compresion</div>
-  )
-}
-
-export default Posicion_compresion
